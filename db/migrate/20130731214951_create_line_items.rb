5fa859274b1abd5d745dc54992096e04bd807478
Ryan Krug <ryan@elbongurk.com>
2013-08-23 17:05:38 -0500

    Changes to remove unneeded unique key constraint

--- db/migrate/20130731214951_create_line_items.rb
+++ db/migrate/20130731214951_create_line_items.rb
@@ -6,8 +6,6 @@ class CreateLineItems < ActiveRecord::Migration
       t.uuid :photo_id, null: false
       t.integer :quantity, default: 1
     end
-
-    add_index :line_items, [:cart_id, :product_id], unique: true
   end
 
   def down
