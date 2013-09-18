f5c6b35db983ede1bdb89babc828a52eba6f23ed
Ryan Krug <ryan@elbongurk.com>
2013-09-18 17:09:16 -0500

    Changing to recommended smtp settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -5,6 +5,6 @@ if Rails.env.staging? || Rails.env.production?
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: :login,
-    enable_starttls_auto: true
+    port: '25'
   }
 end
