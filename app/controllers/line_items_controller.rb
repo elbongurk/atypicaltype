72872d63fc0348a23ff609bc81e7f095dd66c359
Ryan Krug <ryan@elbongurk.com>
2013-10-13 15:50:51 -0700

    Working with multiple canvas sizes.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -6,7 +6,12 @@ class LineItemsController < ApplicationController
   end
 
   def create
-    current_user.cart.line_items.create(create_params)
+    line_item = current_user.cart.line_items.new
+
+    line_item.photo = Photo.find(params[:line_item][:photo_id])
+    line_item.variant = Variant.from_param(params[:line_item][:variant][:sku])
+    
+    line_item.save!
 
     redirect_to cart_url
   end
@@ -14,7 +19,7 @@ class LineItemsController < ApplicationController
   def destroy
     line_item = current_user.cart.line_items.find(params[:id])
     
-    line_item.destroy
+    line_item.destroy!
 
     redirect_to cart_url
   end
@@ -22,18 +27,16 @@ class LineItemsController < ApplicationController
   def update
     line_item = current_user.cart.line_items.find(params[:id])
 
-    line_item.update(update_params)
+    if params[:line_item][:quantity].present?
+      line_item.quantity = params[:line_item][:quantity]
+    end
+    
+    if params[:line_item][:variant].present?
+      line_item.variant = Variant.from_param(params[:line_item][:variant][:sku])
+    end
+    
+    line_item.save!
 
     redirect_to cart_url
-  end
-
-  private
-
-  def update_params
-    params.require(:line_item).permit(:quantity, :variant_id)
-  end
-
-  def create_params
-    params.require(:line_item).permit(:photo_id, :variant_id)
   end
 end
